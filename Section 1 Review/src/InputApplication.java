c9d2a1e6e0e451ea3dde0bdca224ea1774e2a1c4
kchu17 <39917225+kchu17@users.noreply.github.com>
2018-06-13 10:03:52 -0700

    Finished Problem 1 and 2

---
+++ Section 1 Review/src/InputApplication.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class InputApplication 
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a your favoirte dessert: ");
+		String answer = in.nextLine();
+		System.out.println(answer.substring(answer.length()-2, answer.length()-1));
+		Scanner in2 = new Scanner(System.in);
+		System.out.println("Enter a ");
+		
+		
+	}
+}
