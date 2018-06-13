a07a042097094cf27b4252aab3a405534b2bacc0
kchu17 <39917225+kchu17@users.noreply.github.com>
2018-06-13 10:24:21 -0700

    Updated Input Class

--- Section 1 Review/src/InputApplication.java
+++ Section 1 Review/src/InputApplication.java
@@ -5,12 +5,16 @@ public class InputApplication
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter a your favoirte dessert: ");
+		System.out.print("Enter a your favorite dessert: ");
 		String answer = in.nextLine();
-		System.out.println(answer.substring(answer.length()-2, answer.length()-1));
-		Scanner in2 = new Scanner(System.in);
-		System.out.println("Enter a ");
-		
+		System.out.println(answer.substring(answer.length()-1));
+		System.out.print("Enter a double: ");
+		double answer2 = in.nextDouble();
+		System.out.print("Enter an integer: ");
+		int answer3 = in.nextInt();
+		double product = answer2 * answer3;
+		System.out.println("Product: " +  product);
+		System.out.print("Integer portion: " + (int) product);
 		
 	}
 }
